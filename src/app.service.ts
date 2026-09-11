c367455117d77edbd73f4d774b110a4a790e1450
mohamed 3rafa <mohamed3rafa01@gmail.com>
2026-09-11 21:08:33 +0300

    feat: add AppService with initial greeting method

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'SpendSync backend service is running!';
   }
 }
